8a6100478f6e5af73f711bb369245e57da1148db
teoshibin <62151367+teoshibin@users.noreply.github.com>
2026-01-15 14:41:26 +0000

    ssh bgcolor

--- wezterm/options.lua
+++ wezterm/options.lua
@@ -4,6 +4,33 @@ M = {}
 local wezterm = require("wezterm")
 local platform = require("lib.platform")
 
+
+local function configure_colorscheme(config)
+    DEFAULT_THEME = "Catppuccin Mocha"
+    SSH_THEME = "Cobalt2"
+
+    if platform.isMac() then
+        config.window_background_opacity = 0.9
+    else
+        config.window_background_opacity = 1.0
+    end
+
+    config.color_scheme = DEFAULT_THEME
+
+    wezterm.on('update-status',
+        function(window, pane)
+            local fg_process_name = pane:get_foreground_process_name()
+            local overrides = window:get_config_overrides() or {}
+            if fg_process_name == '/usr/bin/ssh' then
+                overrides.color_scheme = SSH_THEME
+            else
+                overrides.color_scheme = DEFAULT_THEME
+            end
+            window:set_config_overrides(overrides)
+        end
+    )
+end
+
 function M.configure(config)
     config.automatically_reload_config = false
     config.max_fps = 120
@@ -24,12 +51,7 @@ function M.configure(config)
     config.line_height = 0.9
 
     -- theme
-    config.color_scheme = "Catppuccin Mocha"
-    if platform.isMac() then
-        config.window_background_opacity = 0.9
-    else
-        config.window_background_opacity = 1.0
-    end
+    configure_colorscheme(config)
 
     -- tab
     config.use_fancy_tab_bar = false
